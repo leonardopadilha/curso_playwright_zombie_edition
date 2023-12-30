19436958c4d18840388a9897567653a5dca5214b
leonardopadilha <leonardopadilha28@gmail.com>
2023-12-30 06:30:37 -0300

    Movies.spec

---
+++ projects/zombieplus/tests/e2e/movies.spec.js
@@ -0,0 +1,26 @@
+const { test } = require('@playwright/test')
+
+const { LoginPage } = require('../pages/LoginPage');
+const { MoviesPage } = require('../pages/MoviesPage')
+const { Toast } = require('../pages/Components');
+
+const email = 'admin@zombieplus.com';
+const password = 'pwd123';
+
+let loginPage;
+let moviesPage;
+let toast;
+
+test.beforeEach(({ page }) => {
+    loginPage = new LoginPage(page)
+    moviesPage = new MoviesPage(page)
+    toast = new Toast(page)
+})
+
+test('deve poder cadastrar um novo filme', async ({ page }) => {
+    
+    // é importante estar logado
+    await loginPage.visit()
+    await loginPage.submit(email, password)
+    await moviesPage.isLoggedIn()
+})
